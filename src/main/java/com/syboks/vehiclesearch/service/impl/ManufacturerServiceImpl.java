af756e087989337b494b0b65cec68f13cf78091a
syboks <nhidzab@gmail.com>
2024-05-18 01:04:20 +0200

    Added restpoint to get all manufacturers

--- src/main/java/com/syboks/vehiclesearch/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/com/syboks/vehiclesearch/service/impl/ManufacturerServiceImpl.java
@@ -6,6 +6,8 @@ import com.syboks.vehiclesearch.service.ManufacturerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class ManufacturerServiceImpl implements ManufacturerService {
@@ -14,5 +16,11 @@ public class ManufacturerServiceImpl implements ManufacturerService {
     @Override
     public Manufacturer saveManufacturer(Manufacturer manufacturer) {
         return manufacturerDAO.save(manufacturer);
+    }
+
+    @Override
+    public List<Manufacturer> fetchAllManufacturers() {
+        List<Manufacturer> db=manufacturerDAO.findAll();
+        return db;
     }
 }
