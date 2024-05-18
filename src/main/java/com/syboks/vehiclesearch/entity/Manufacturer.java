fc3b1635fed7aa5a181fd21adc96a0c23600fb4a
syboks <nhidzab@gmail.com>
2024-05-18 10:14:24 +0200

    created update manufacturer endpoint

--- src/main/java/com/syboks/vehiclesearch/entity/Manufacturer.java
+++ src/main/java/com/syboks/vehiclesearch/entity/Manufacturer.java
@@ -2,6 +2,7 @@ package com.syboks.vehiclesearch.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class Manufacturer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Manufacturer name is required")
     @Column(name="manufacturer_name")
     private String manufacturerName;
     @Column(name="country_of_origin")
--- src/main/java/com/syboks/vehiclesearch/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/com/syboks/vehiclesearch/service/impl/ManufacturerServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -32,5 +33,20 @@ public class ManufacturerServiceImpl implements ManufacturerService {
             return dbManufacturer.get();
         }
         return null;
+    }
+
+    @Override
+    public Manufacturer updateManufacturer(int id, Manufacturer updatedManufacturer) {
+        Manufacturer dbManufacturer=getManufacturerForId(id);
+        if(dbManufacturer!=null && Objects.nonNull(updatedManufacturer)){
+            if(!"".equalsIgnoreCase(updatedManufacturer.getManufacturerName())){
+                dbManufacturer.setManufacturerName(updatedManufacturer.getManufacturerName());
+            }
+            if(!"".equalsIgnoreCase(updatedManufacturer.getCountryOfOrigin())){
+                dbManufacturer.setCountryOfOrigin(updatedManufacturer.getCountryOfOrigin());
+            }
+            return manufacturerDAO.save(dbManufacturer);
+        }
+        return dbManufacturer;
     }
 }
